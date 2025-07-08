24db6cde936d00bbe3e1e644eade224f44332fcf
AsimMahmudov <mahmudovasim799@gmail.com>
2025-07-08 21:19:00 +0600

    test4

--- src/app/[locale]/(home)/page.tsx
+++ src/app/[locale]/(home)/page.tsx
@@ -1,13 +1,13 @@
-import Applications from "@/components/business/applications/Applications";
-import BusinessProblems from "@/components/business/business-problems/BusinessProblems";
-// import Cards from "@/components/business/cards/Cards";
-import Hero from "@/components/business/hero/Hero";
-import Instruction from "@/components/business/instruction/Instruction";
-// import Order from "@/components/business/order/Order";
-import Service from "@/components/business/service/Service";
-// import Shop from "@/components/business/shop/Shop";
-// import Group from "@/components/client/group/Group";
-// import WeTeam from "@/components/client/we-team/WeTeam";
+// import Applications from "@/components/business/applications/Applications";
+// import BusinessProblems from "@/components/business/business-problems/BusinessProblems";
+import Cards from "@/components/business/cards/Cards";
+// import Hero from "@/components/business/hero/Hero";
+// import Instruction from "@/components/business/instruction/Instruction";
+import Order from "@/components/business/order/Order";
+// import Service from "@/components/business/service/Service";
+import Shop from "@/components/business/shop/Shop";
+import Group from "@/components/client/group/Group";
+import WeTeam from "@/components/client/we-team/WeTeam";
 import { generateMetadata } from "@/lib/seo";
 import React from "react";
 
@@ -22,25 +22,25 @@ export const metadata = generateMetadata({
 const Home = () => {
 	return (
 		<>
-			<Hero />
-			<Service />
-			<BusinessProblems />
-			<Instruction />
-			<Applications />
+			{/* <Hero /> */}
+			{/* <Service /> */}
+			{/* <BusinessProblems /> */}
+			{/* <Instruction /> */}
+			{/* <Applications /> */}
 
 			{/* // */}
 			{/* <Reviews /> */}
 
 			{/* // */}
 
-			{/* <Order /> */}
-			{/* <Shop /> */}
-			{/* <Cards /> */}
+			<Order />
+			<Shop />
+			<Cards />
 
 			{/* // */}
 
-			{/* <Group /> */}
-			{/* <WeTeam /> */}
+			<Group />
+			<WeTeam />
 		</>
 	);
 };
