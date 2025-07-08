0563d744f326e261407b12a626c4931ca0533928
AsimMahmudov <mahmudovasim799@gmail.com>
2025-07-08 22:02:42 +0600

    test8

--- src/components/business/applications/Applications.tsx
+++ src/components/business/applications/Applications.tsx
@@ -1,12 +1,8 @@
 "use client";
 import { Title } from "@/components/ui/text/Title";
-// import img1 from "@/assets/images/applications1cl.png";
+import img1 from "@/assets/images/applications3cl_new2.jpg";
 import img2 from "@/assets/images/applications2cl.png";
 import img3 from "@/assets/images/applications3cl.png";
-
-import img1 from "@/assets/images/men.png";
- 
-
 
 import Image from "next/image";
 import { useTranslations } from "next-intl";
@@ -41,9 +37,9 @@ const Applications = () => {
 							data-aos-delay={index * 200}
 							className=" overflow-hidden bg-[#F2F2F2] rounded-[20px] w-full h-[400px] flex justify-center items-end">
 							<div className=" relative w-[233px] h-[330px]">
-								<div className="w-[222px] h-[330px] bg-[#DDDDDD] absolute right-4 rounded-tr-[20px] rounded-tl-[20px]" />
+								<div className="w-[222px] h-[330px] bg-[#DDDDDD] absolute right-4 rounded-tr-[20px] rounded-tl-[20px] " />
 								<Image
-									className="mt-3"
+									className="mt-3 rounded-tr-[30px] rounded-tl-[30px]"
 									fill
 									objectFit="contain"
 									src={el.img}
