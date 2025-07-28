c8821b26f264ce60cfe2947881b72c3c2646b05d
AsimMahmudov <mahmudovasim799@gmail.com>
2025-07-28 16:50:19 +0600

    add yandex metrika

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import LayoutPage from "@/components/navbar/LayoutPage";
 import { NextIntlClientProvider } from "next-intl";
 import { getMessages } from "next-intl/server";
 import { SITE_DESCRIPTION, SITE_NAME } from "@/constants/seo.constants";
+import Script from "next/script";
 
 const geistSans = localFont({
 	src: "./fonts/GeistVF.woff",
@@ -38,11 +39,47 @@ export default async function RootLayout({
 
 	return (
 		<html lang={locale}>
+			<head>
+				{/* Yandex Metrika Script */}
+				<Script
+					id="yandex-metrika"
+					strategy="afterInteractive"
+					dangerouslySetInnerHTML={{
+						__html: `
+							(function(m,e,t,r,i,k,a){
+								m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+								m[i].l=1*new Date();
+								for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+								k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
+							})(window, document, 'script', 'https://mc.yandex.ru/metrika/tag.js?id=103506111', 'ym');
+							ym(103506111, 'init', {
+								ssr:true,
+								webvisor:true,
+								clickmap:true,
+								ecommerce:"dataLayer",
+								accurateTrackBounce:true,
+								trackLinks:true
+							});
+						`,
+					}}
+				/>
+			</head>
 			<body
 				className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
 				<NextIntlClientProvider messages={messages}>
 					<LayoutPage>{children}</LayoutPage>
 				</NextIntlClientProvider>
+
+				{/* Yandex Metrika noscript */}
+				<noscript>
+					<div>
+						<img
+							src="https://mc.yandex.ru/watch/103506111"
+							style={{ position: "absolute", left: "-9999px" }}
+							alt=""
+						/>
+					</div>
+				</noscript>
 			</body>
 		</html>
 	);
