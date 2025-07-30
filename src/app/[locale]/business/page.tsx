9487a0eacad33dac2fa112da16bd899338570799
Dastan Mukeev <55836145+thedastan@users.noreply.github.com>
2025-07-30 16:56:17 +0600

    Update page.tsx

--- src/app/[locale]/business/page.tsx
+++ src/app/[locale]/business/page.tsx
@@ -9,11 +9,11 @@ import { generateMetadata } from "@/lib/seo";
 import React from "react";
 
 export const metadata = generateMetadata({
-	title: "TezKyzmat — Информационный сайт для продавцов автозапчастей",
+	title: "TK Business — Заявки для вашего автобизнеса",
 	description:
-		"Узнайте, как продавцам автозапчастей использовать TezKyzmat для расширения аудитории и удобного взаимодействия с клиентами.",
-	url: "https://tezkyzmat-web.vercel.app/",
-	image: "https://tezkyzmat-web.vercel.app/seo-img.png",
+		"Узнайте, как продавцам автозапчастей использовать TK Business для расширения аудитории и удобного взаимодействия с клиентами.",
+	url: "https://tezkyzmat.kg/ru/business",
+	image: "https://tezkyzmat.kg/seo-img.png",
 });
 
 const BusinessPage = () => {
